d755e9a3d0bdcc3568a0784b610594ee0953fc86
gan0312 <105478447+gan0312@users.noreply.github.com>
2022-08-12 16:51:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wx761148a149b09d1b"
 # 公众号appSecret
-app_secret = ""
+app_secret = "1d291367611ec3e3b123eb0a07514648"
 # 模板消息id
-# template_id = ""
+template_id = "iMxHQlQ9YT1iuZ6yK3v1H0CWfSBw9GyqHWzRfob5jmc"
 # 接收公众号消息的微信号
-user = ""
+user = "oo6lB5-41yTPI_fk8PKu3NR54BbQ"
 
 # 信息配置
 # 所在省份
 province = "广东"
 # 所在城市
-city = "广州"
+city = "深圳"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
 birthday = "1997-1-1"
 # 在一起的日子，格式同上
